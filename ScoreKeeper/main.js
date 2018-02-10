ca88aa85c56596001a20266f50eeaf615bc12c29
Serghei Tibulschii <Serghei.Tibulschii@endava.com>
2018-02-10 23:28:30 +0200

    job on score keeper is done

--- ScoreKeeper/main.js
+++ ScoreKeeper/main.js
@@ -1,6 +1,9 @@
 $(document).ready(() => {
+    $("#counter").val(0);
     var $player1 = $("#player1");
     var $player2 = $("#player2");
+    var $reset = $("#reset");
+    var limit = 0;
 
     var $num1 = $("#num1");
     var $num2 = $("#num2");
@@ -11,13 +14,37 @@ $(document).ready(() => {
     $(num1).text(counter1);
     $(num2).text(counter2);
 
+    $reset.click(() => reset());
+
     $player1.on("click", () => {
-        counter1++;
-        $(num1).text(counter1);
+        if (counter1 === limit) return;
+
+        ++counter1 === limit && $num1.addClass("textGreen");
+        $num1.text(counter1);
     });
 
     $player2.on("click", () => {
-        counter2++;
-        $(num2).text(counter2);
+        if (counter2 === limit) return;
+
+        ++counter2 === limit && $num2.addClass("textGreen");
+
+        $num2.text(counter2);
     });
+
+    $("#counter").change(function() {
+        $(".badge").text(this.value);
+        limit = +this.value;
+    });
+
+    function reset() {
+        $(".nums").each((index, num) => {
+            $(num)
+                .text(0)
+                .removeClass("textGreen");
+        });
+
+        counter1 = counter2 = limit = 0;
+        $("#counter").val(0);
+        $(".badge").text(0);
+    }
 });
