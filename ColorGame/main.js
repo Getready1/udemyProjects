f61ee26b22570c2cc3df9697cfd7e42918554139
Serghei Tibulschii <Serghei.Tibulschii@endava.com>
2018-02-11 13:17:40 +0200

    added lvl difficulty

--- ColorGame/main.js
+++ ColorGame/main.js
@@ -8,6 +8,102 @@ function getRandomRGBElement() {
     return `rgb(${result.join(",")})`;
 }
 
-$(".color").each((index, element) => {
-    element.style.backgroundColor = getRandomRGBElement();
+var lvls = {
+    easy: {
+        items: 3,
+        rows: 1
+    },
+    hard: {
+        items: 3,
+        rows: 3
+    }
+};
+
+var currentLvl = lvls.easy;
+
+$("#easy-btn").on("click", () => {
+    document.getElementById("color-wrapper").innerHTML = "";
+    initItems(lvls.easy);
 });
+
+$("#hard-btn").on("click", () => {
+    document.getElementById("color-wrapper").innerHTML = "";
+    initItems(lvls.hard);
+});
+
+initGame();
+initColors();
+initItems(currentLvl);
+
+$("#newColors").on("click", () => {
+    initColors();
+});
+
+function initGame() {}
+
+function initColors() {
+    $(".flex-item").each((index, element) => {
+        element.style.backgroundColor = getRandomRGBElement();
+    });
+}
+
+function initItems(lvl) {
+    var rows = getRows(lvl.rows);
+    var rowContainer = document.getElementById("color-wrapper");
+
+    rows.forEach(row => {
+        var items = getItems(lvl.items);
+        items.forEach((item, index) => {
+            row.appendChild(item);
+        });
+
+        $(rowContainer).append(row);
+    });
+}
+
+function getItems(itemsCount) {
+    var items = [],
+        i;
+
+    for (i = itemsCount; i--;) {
+        items.push(
+            createElement({
+                tag: "div",
+                class: "flex-item",
+                styles: {
+                    "background-color": getRandomRGBElement()
+                }
+            })
+        );
+    }
+
+    return items;
+}
+
+function getRows(rowCount) {
+    var rows = [],
+        i;
+    for (i = rowCount; i--;) {
+        rows.push(
+            createElement({
+                tag: "div",
+                class: "flex-row"
+            })
+        );
+    }
+
+    return rows;
+}
+
+function createElement(props) {
+    var element = document.createElement(props.tag);
+    element.className = props.class;
+    if (props.styles) stylizeElement(element, props.styles);
+    return element;
+}
+
+function stylizeElement(element, styles) {
+    Object.keys(styles).forEach(styleKey => {
+        element.style[styleKey] = styles[styleKey];
+    });
+}
